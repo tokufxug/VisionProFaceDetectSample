7bcc737cbf4b4092dcaf73c6171a91724c6cc6de
tokufxug <tokufxug@gmail.com>
2024-03-27 22:18:12 +0900

    Initial Commit

---
+++ VisionProFaceDetectSample/VisionProFaceDetectSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProFaceDetectSampleApp.swift
+//  VisionProFaceDetectSample
+//
+//  Created by Sadao Tokuyama on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionProFaceDetectSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
